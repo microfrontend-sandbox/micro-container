09c32d6d8c70c4a51ba8e124ca4e2ec595fd3375
Sean Malone <sean.malone@projekt202.com>
2020-06-03 14:04:39 -0500

    Adds store

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+render();
+
+if (process.env.NODE_ENV === 'development' && (module as any).hot) {
+  (module as any).hot.accept('./store', () => {
+    store.replaceReducer(require('./store').default);
+  });
+  (module as any).hot.accept('./App', render)
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
